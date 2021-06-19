d91cbdc8f356d32169875113624a396901aabfc0
tocchimittsu <arimagedon21@gmail.com>
2021-06-20 05:20:27 +0900

    修正

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ import tkinter as tk
 
 
 class Application(tk.Frame):
-    # 現在選択されているボタンの値を格納する変数
+    # 現在選択されているラジオボタンの値を格納する変数
     variable = None
-    # Aラジオボタンを格納する変数
-    aButton = None
-    # Bラジオボタンを格納する変数
-    bButton = None
-    # Cラジオボタンを格納する変数
-    cButton = None
 
     # 現在選択されているラジオボタンの値を確認する関数
     def showValue(self):
@@ -28,21 +22,21 @@ class Application(tk.Frame):
         # frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
         frame = tk.Frame(self.master)
 
-        # Windowを親要素とした場合に、frame Widgetをどのように配置するのか?
+        # Windowを親要素とした場合に、frame Widget(Frame)をどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         frame.pack()
 
-        # stringを格納するように初期化。
+        # 現在選択されているラジオボタンの値を文字列変数として扱う。
         # StringVarについて : https://kuroro.blog/python/K53voPjJuKFfYrjmP8FP/
         self.variable = tk.StringVar()
-        # set() : 引数に該当するラジオボタンを選択する。初期値としてBの値を持つラジオボタンを選択。
+        # set() : 引数の値に該当するラジオボタンを選択する。初期値としてBの値を持つラジオボタンを選択。
         self.variable.set('B')
 
         # frame Widget(Frame)を親要素として、Aのradiobutton Widgetを作成する。
-        # variable : 現在利用中のラジオボタンの値を格納する。その他のラジオボタンへ値を共有するために利用する。
-        # value : ラジオボタン自身が持つ値。Aとする。
+        # variable : 現在選択中のラジオボタンの値を格納する。文字列変数(self.variable)として値を持たせることで、可変として扱い、その他のラジオボタンへ値を共有して選択の状態を管理できる。
+        # value : ラジオボタン自身が持つ値の設定。Aとする。
         # text : ラジオボタンを説明するテキスト。Option Aとする。
-        # command : ラジオボタンが選択された場合に実行する関数を設定する。showValue関数を設定する。
+        # command : ラジオボタンが選択された場合に実行する関数を設定する。self.showValue関数を設定する。
         self.aButton = tk.Radiobutton(frame, variable=self.variable, value='A', text='Option A', command=self.showValue)
 
         # frame Widget(Frame)を親要素とした場合に、Aのradiobutton Widgetをどのように配置するのか?
@@ -50,10 +44,10 @@ class Application(tk.Frame):
         self.aButton.pack()
 
         # frame Widget(Frame)を親要素として、Bのradiobutton Widgetを作成する。
-        # variable : 現在利用中のラジオボタンの値を格納する。その他のラジオボタンへ値を共有するために利用する。
-        # value : ラジオボタン自身が持つ値。Bとする。
+        # variable : 現在選択中のラジオボタンの値を格納する。文字列変数(self.variable)として値を持たせることで、可変として扱い、その他のラジオボタンへ値を共有して選択の状態を管理できる。
+        # value : ラジオボタン自身が持つ値の設定。Bとする。
         # text : ラジオボタンを説明するテキスト。Option Bとする。
-        # command : ラジオボタンが選択された場合に実行する関数を設定する。showValue関数を設定する。
+        # command : ラジオボタンが選択された場合に実行する関数を設定する。self.showValue関数を設定する。
         self.bButton = tk.Radiobutton(frame, variable=self.variable, value='B', text='Option B', command=self.showValue)
 
         # frame Widget(Frame)を親要素とした場合に、Bのradiobutton Widgetをどのように配置するのか?
@@ -61,15 +55,26 @@ class Application(tk.Frame):
         self.bButton.pack()
 
         # frame Widget(Frame)を親要素として、Cのradiobutton Widgetを作成する。
-        # variable : 現在利用中のラジオボタンの値を格納する。その他のラジオボタンへ値を共有するために利用する。
-        # value : ラジオボタン自身が持つ値。Cとする。
+        # variable : 現在選択中のラジオボタンの値を格納する。文字列変数(self.variable)として値を持たせることで、可変として扱い、その他のラジオボタンへ値を共有して選択の状態を管理できる。
+        # value : ラジオボタン自身が持つ値の設定。Cとする。
         # text : ラジオボタンを説明するテキスト。Option Cとする。
-        # command : ラジオボタンが選択された場合に実行する関数を設定する。showValue関数を設定する。
+        # command : ラジオボタンが選択された場合に実行する関数を設定する。self.showValue関数を設定する。
         self.cButton = tk.Radiobutton(frame, variable=self.variable, value='C', text='Option C', command=self.showValue)
 
         # frame Widget(Frame)を親要素とした場合に、Cのradiobutton Widgetをどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         self.cButton.pack()
+
+        # frame Widget(Frame)を親要素として、dのradiobutton Widgetを作成する。
+        # variable : 現在選択中のラジオボタンの値を格納する。文字列変数(self.variable)として値を持たせることで、可変として扱い、その他のラジオボタンへ値を共有して選択の状態を管理できる。
+        # value : ラジオボタン自身が持つ値の設定。Cとする。
+        # text : ラジオボタンを説明するテキスト。Option Dとする。
+        # command : ラジオボタンが選択された場合に実行する関数を設定する。self.showValue関数を設定する。
+        self.dButton = tk.Radiobutton(frame, variable=self.variable, value='C', text='Option D', command=self.showValue)
+
+        # frame Widget(Frame)を親要素とした場合に、Dのradiobutton Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        self.dButton.pack()
 
 # Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
 if __name__ == "__main__":
